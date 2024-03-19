1b1fb4f5ebdc65ddb6a1b26c5d59359f6bff7afe
Daniel Dąbrowski <danieldabrowskikontakt@gmail.com>
2024-03-19 12:49:01 +0100

    init okta

--- src/main/java/com/example/booktradeuserservice/controller/TestController.java
+++ src/main/java/com/example/booktradeuserservice/controller/TestController.java
@@ -1,12 +1,15 @@
 package com.example.booktradeuserservice.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 public class TestController {
 
-    @GetMapping("/test")
-    ResponseEntity test(){
-        return ResponseEntity.ok().build();
+    @PostMapping("/test")
+    public String home(@AuthenticationPrincipal OidcUser user) {
+        return "Welcome, "+ user.getFullName() + "!";
     }
 }
--- src/main/java/com/example/booktradeuserservice/security/SecurityConfig.java
+++ src/main/java/com/example/booktradeuserservice/security/SecurityConfig.java
@@ -1,24 +1,51 @@
 package com.example.booktradeuserservice.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.IOException;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SessionRegistry sessionRegistry() {
-        return new SessionRegistryImpl();
-    }
+    @Value("${okta.oauth2.issuer}")
+    private String issuer;
+    @Value("${okta.oauth2.client-id}")
+    private String clientId;
 
     @Bean
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(sessionRegistry());
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(withDefaults())
+                .logout(logout -> logout
+                .addLogoutHandler(logoutHandler()));
+        return http.build();
+    }
+
+    private LogoutHandler logoutHandler() {
+        return (request, response, authentication) -> {
+            try {
+                String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+                response.sendRedirect(issuer + "v2/logout?client_id=" + clientId + "&returnTo=" + baseUrl);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
     }
 }
