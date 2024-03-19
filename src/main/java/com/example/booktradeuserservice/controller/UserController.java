1664f5fe6b3c5d869711de7470501a037951bd78
Daniel Dąbrowski <danieldabrowskikontakt@gmail.com>
2024-03-19 16:06:58 +0100

    Login and Logout endpoints

--- src/main/java/com/example/booktradeuserservice/controller/UserController.java
+++ src/main/java/com/example/booktradeuserservice/controller/UserController.java
@@ -5,18 +5,20 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
+public class UserController {
 
-    @GetMapping("/test")
-    public ResponseEntity<OidcUserInfo> home(@AuthenticationPrincipal OidcUser user) {
+
+    @GetMapping("/oauth2/authorization/okta")
+    public ResponseEntity<OidcUserInfo> login(@AuthenticationPrincipal OidcUser user){
         return ResponseEntity.ok(user.getUserInfo());
-
     }
-    @GetMapping("/test/test/test")
-    public ResponseEntity<OidcUserInfo> home2(@AuthenticationPrincipal OidcUser user) {
+
+    @PostMapping("/logout")
+    public ResponseEntity<OidcUserInfo> logout(@AuthenticationPrincipal OidcUser user){
         return ResponseEntity.ok(user.getUserInfo());
     }
 }
